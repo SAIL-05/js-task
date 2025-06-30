190bc460f1d6b01552708c5d88c2c662d84c7001
Muhammed Okoro Ibrahim <mi.fracri91@gmail.com>
2025-06-30 16:02:58 +0100

    update code

--- playground/src/App.jsx
+++ playground/src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  const [count, setCount] = useState(0);
+
+  const change = () => {
+    setCount(count + 1);
+  };
+
+  const finalCount = () => {
+    setCount(count * 2);
+  };
+
+  return (
+    <>
+      <h1>This is a counter</h1>
+      <p>counter: {count}</p>
+      <button onClick={change}>click</button>
+      <button onClick={finalCount}>final</button>
+    </>
+  );
 };
 
 export default App;
